b5d75e36f3e2ec451e8cd7753daed5761f42e418
oolong32 <josef.renner@gmail.com>
2024-10-20 23:02:43 +0200

    insert semicolon 😅

--- phenomenon.js
+++ phenomenon.js
@@ -2,10 +2,10 @@ class Phenomenon {
   constructor(x, y, s) {
     this.x = x;
     this.y = y;
-    this.size = s
+    this.size = s;
   }
 
   show() {
-    ellipse(this.x, this.y, this.size)
+    ellipse(this.x, this.y, this.size);
   }
 }
