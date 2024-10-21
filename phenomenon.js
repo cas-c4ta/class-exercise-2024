c8fbe892284de257421a07ccd9aefd597b78b88f
oolong32 <josef.renner@gmail.com>
2024-10-21 14:39:40 +0200

    update class, add comparision

--- phenomenon.js
+++ phenomenon.js
@@ -1,36 +1,80 @@
+
+
 class Phenomenon {
   constructor(x, y, s) {
-    this.orbitRadius = 10 // wie wird das bestimmt?
-    this.orbitCenter = {cx, cy} // wie bestimmen wir das?
+    // this.orbitRadius = random(30, 60) // Zufallsradius
+    this.orbitRadius = 0
+
+    this.orbitCenter = {x: 200, y:200} // wie bestimmen wir das?
+
     // wie bestimmt man das?
     this.orbitAngle = random(2*PI) // wie wird das bestimmt?
+    // console.log(this.orbitAngle)
     this.orbitSpeed = 0.01 // wie wird das bestimmt?
-    this.x = this.orbitRadius * cos(this.orbitAngle) 
-    this.y = this.orbitRadius * sin(this.orbitAngle) 
 
     this.shapeOptions = ["circle", "square", "triangle"]
     this.shape = this.shapeOptions[floor(random(this.shapeOptions.length))]
     this.size = s;
+    
+    // Mel’s Code
+    this.x = x;
+    this.y = y;
+    let cx = null;
+    let cy = null;
+
+    // Define Orbit Center
+    if (0 <= this.orbitAngle <= PI / 2 || 3 * PI / 2 <= this.orbitAngle <= TWO_PI) {
+      // Winkel ist zwischen Null und 90 oder zwischen 270 und 360
+      cx = this.x - this.orbitRadius * cos(this.orbitAngle)
+    } else {
+      cx = this.x + this.orbitRadius * cos(this.orbitAngle)
+    }
+    if (0 <= this.orbitAngle <= PI) { // Winkel 0 und 180 grad
+      cy = this.y - this.orbitRadius * sin(this.orbitAngle)
+    } else {
+      cy = this.y + this.orbitRadius * sin(this.orbitAngle)
+    }
+    this.orbitCenter = { cx, cy } 
+
   }
 
   move() {
     // this.orbitAngle muss wachsen, um this.orbitSpeed
     this.orbitAngle += this.orbitSpeed
-    this.x = this.orbitRadius * cos(this.orbitAngle) 
-    this.y = this.orbitRadius * sin(this.orbitAngle) 
+    this.x = this.orbitCenter.cx + this.orbitRadius * cos(this.orbitAngle) 
+    this.y = this.orbitCenter.cy + this.orbitRadius * sin(this.orbitAngle) 
+  }
+
+  mapMouseY() {
+     this.orbitRadius = map(mouseY, 0, height, 30, 60) // abhängig von Cursor
+  }
+
+  showOrbit() {
+    // center
+    push()
+    fill(255, 0, 0)
+    noStroke()
+    ellipse(this.orbitCenter.cx, this.orbitCenter.cy, 4)
+    stroke(255, 0, 0)
+    noFill()
+    ellipse(this.orbitCenter.cx, this.orbitCenter.cy, this.orbitRadius*2)
+    pop()
   }
 
   show() {
-    fill(this.redValue, this.greenValue, this.blueValue)
+    // fill(this.redValue, this.greenValue, this.blueValue)
+    // Task:
+    // Objekte auf Orbit zentrieren
     if (this.shape == "circle") {
       ellipse(this.x, this.y, this.size);
     } else if (this.shape == "square") {
       square(this.x, this.y, this.size);
     } else { // triangle
-      triangle(this.x, this.y,
-        this.x + this.size, this.y,
-        this.x, this.y + this.size
-        // task: verschönern bei Gelegenheit
+      const tes = this.size/12 // tes = triangle extra sauce (typographic geek stuff)
+      triangle(
+        this.x - this.size/2 - tes, this.y - this.size/2,
+        this.x + this.size/2 + tes, this.y - this.size/2,
+        this.x, this.y + this.size/2
       )
     }
   }
--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let phens = []
 
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER)
   for (let i = 0; i < 10; i++) {
     phens.push(new Phenomenon(random(400), random(400), 40));
   }
@@ -19,6 +20,31 @@ function setup() {
 function draw() {
   background(120);
   for (let phen of phens) {
+    phen.mapMouseY()
+    phen.move()
     phen.show()
+    // phen.showOrbit()
+  }
+  compareDistances()
+}
+
+function compareDistances() {
+
+  for (let i = 0; i < phens.length - 1; i += 1) {
+    let currentX = phens[i].x
+    let currentY= phens[i].y
+    // compare rest of phens
+    for (let j = i + 1; j < phens.length; j += 1) {
+      let nextX = phens[j].x
+      let nextY = phens[j].y
+      let distPhens = dist(currentX, currentY, nextX, nextY)
+      // draw Line if short distance
+      if (distPhens < 100) {
+        push()
+        stroke(255)
+        line(currentX, currentY, nextX, nextY)
+        pop()
+      }
+    }
   }
 }
